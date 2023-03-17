3f1b89e4458c908087bf2ba28589b3e1fdbd1791
Herik <herik.reis02@gmail.com>
2023-03-17 15:59:56 -0300

    associação com os erros pelo tf-idf

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
 import json
+import pandas as pd
+import numpy as np
 from model.log import Log_chat
 from model.extract import class_prediction, get_response
 from keras.models import load_model
@@ -9,7 +11,6 @@ from random import choice
 
 def chatbot_run(input_user):
     input_user = Tratamento.preprocess_input(input_user)
-    print(input_user)
     id_response = 0
     # extrai o modelo usando o keras
     # model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Chatbot\\model\\model.h5"))
@@ -23,10 +24,21 @@ def chatbot_run(input_user):
     with open(json_censored_path,'r',encoding="UTF-8") as banco:
         intents = json.load(banco)
     censored,id = Correlacao.tf_idf(input_user,intents["intents"][0]["patterns"])
-    if censored > 0.07:
+    if censored > 0.2:
         list_response = intents["intents"][0]["responses"]
         response = choice(list_response)
     else:
+        model_path = "./model/model.h5"
+        model = load_model(model_path)
+        intent_user = class_prediction(input_user, model)
+        if intent_user[0]['intent'] == 'problem_solution':
+            table_problemas = pd.read_csv("database\\tabela_problemas.csv", encoding="iso-8859-1")
+            vetor,id = Correlacao.tf_idf(input_user,table_problemas['problema'].tolist())
+            if vetor > 0.3:
+                response = table_problemas['problema'][id]
+            else:
+                response = get_response([{'intent': 'welcome_problem', 'probability': '1.0'}], intents_db)
+        else:
         # # carrega a intenção do log da última conversa
         # intent_log = ...
         # # verifica se a intenção era um feedback
@@ -37,12 +49,11 @@ def chatbot_run(input_user):
         #     ...
         # # se não for nenhuma das intenções de resposta acima, chama o modelo para interagir
         # else:
-        model_path = "./model/model.h5"
-        model = load_model(model_path)
-        intent_user = class_prediction(input_user, model)
-        response = get_response(intent_user, intents_db)
+            response = get_response(intent_user, intents_db)
     #     list_response = get_response(intent_user, intents_db)
     #     response = [id_response]
     # # cria um log da conversa
     # Log_chat(intent_user,input_user,response,list_response=0,id_response=0)
     return response
+
+a = chatbot_run('olá')
--- test.py
+++ test.py
@@ -1,37 +1,6 @@
+import pandas as pd
 import os
-import json
-from model.log import Log_chat
-from model.extract import class_prediction, get_response
-from keras.models import load_model
-from model.preprocess import Tratamento
-from model.preprocess import Correlacao
-from numpy import delete
-from sklearn.feature_extraction.text import TfidfVectorizer # pip install scikit-learn
-from sklearn.metrics.pairwise import cosine_similarity
-from random import choice
 
-def chatbot_run(input_user):
-    input_user = Tratamento.preprocess_input(input_user)
-    id_response = 0
-    # extrai o modelo usando o keras
-    # model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Chatbot\\model\\model.h5"))
-    model_path = "./model/model.h5"
-    model = load_model(model_path)
-    # abre o banco de dados para carregar as intenções
-    db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "ChatbotSemeq\\database\\intents.json"))
-    with open(db_path,'r',encoding="UTF-8") as banco:
-        intents_db = json.load(banco)
-
-    # armazena a lista de palavras censuradas
-    json_censored_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "ChatbotSemeq\\database\\censored.json"))
-    with open(json_censored_path,'r',encoding="UTF-8") as banco:
-        intents = json.load(banco)
-    censored,id = Correlacao.tf_idf(input_user,intents["intents"][0]["patterns"])
-    if censored > 0.1:
-        list_response = intents["intents"][0]["responses"]
-        response = choice(list_response)
-        print("não vai passar")
-
-a = chatbot_run("vai se fuder")
-print()
-b = chatbot_run("olá")
+csv_censored_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "ChatbotSemeq\\database\\tabela_problemas_csv.csv"))
+dados = pd.read_csv("database\\tabela_problemas_csv.csv", encoding="iso-8859-1")
+print(dados)
