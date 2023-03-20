8ab8b3bab91fb56f1cb4c6bf57644c8c8b30bca5
Herik <herik.reis02@gmail.com>
2023-03-20 14:13:47 -0300

    associação em nivel incompleta

--- filtro.py
+++ filtro.py
@@ -25,26 +25,15 @@ def analysis(input_user):
         classes_path = f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\classes.pkl"
         intent_device = class_prediction(input_user, model_path,words_path,classes_path)
         device = get_response(intent_device, list_device)
-        if device == "anything_else":
-            with open("models\\modelos_intencoes\\anything_else\\intents.json",'r',encoding="UTF-8") as bd:
-                list_intents = json.load(bd)
-            intent_user = [{"intent": "anything_else", "probability": 1.0}]
-            response = get_response(intent_user, list_intents)
-        else:
-            # interface
-            with open(f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\{device}\\intents.json",'r',encoding="UTF-8") as bd:
-                list_device = json.load(bd)
-            model_path = f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\{device}\\model.h5"
-            words_path = f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\{device}\\words.pkl"
-            classes_path = f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\{device}\\classes.pkl"
-            intent_device = class_prediction(input_user, model_path,words_path,classes_path)
-            interface = get_response(intent_device, list_device)
-            if interface == "anything_else":
-                with open("models\\modelos_intencoes\\anything_else\\intents.json",'r',encoding="UTF-8") as bd:
-                    list_intents = json.load(bd)
-                intent_user = [{"intent": "anything_else", "probability": 1.0}]
-                response = get_response(intent_user, list_intents)
-            else:
-                response = interface
+        
+        # interface
+        with open(f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\modelo_interface\\{device}\\intents.json",'r',encoding="UTF-8") as bd:
+            list_device = json.load(bd)
+        model_path = f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\modelo_interface\\{device}\\model.h5"
+        words_path = f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\modelo_interface\\{device}\\words.pkl"
+        classes_path = f"models\\modelos_intencoes\\modelo_solution\\modelo_device\\{subject}\\modelo_interface\\{device}\\classes.pkl"
+        intent_device = class_prediction(input_user, model_path,words_path,classes_path)
+        interface = get_response(intent_device, list_device)
+        response = interface
 
     return subject + " / " + device + " / " + response
